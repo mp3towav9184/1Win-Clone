66ef32c90115b76d3a86b2c9785e5a08322c4e28
zarir94 <shahriarzariracc@gmail.com>
2025-05-28 11:41:55 +0600

    ok

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h3>Hello</h3>
+
+<style lang="postcss">
+  @reference "tailwindcss";
+  h3 { @apply bg-emerald-500; }
+</style>
