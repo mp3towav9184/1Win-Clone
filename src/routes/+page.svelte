4c689c18b6bc2a189d701fe7c94615b340cd026d
zarir94 <shahriarzariracc@gmail.com>
2025-05-28 12:02:47 +0600

    ok

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h3>Hello</h3>
+<style lang="postcss">
+  @reference "tailwindcss";
+  h3 {@apply bg-red-700 text-6xl;}
+</style>
