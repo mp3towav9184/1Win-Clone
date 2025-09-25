a46b6d44ae5f19a827b8cdc174cdfef5c6fdc067
Blackhat40456 <blackhat404t@gmail.com>
2025-09-25 19:13:43 +0600

    Update func.ts

--- src/lib/func.ts
+++ src/lib/func.ts
@@ -12,24 +12,18 @@ export function genCoef() {
 	const a = random.int(1, 1000);
 	let coef: number;
 
-	if (a <= 100) {
-		// 10%: 1.00x – 1.20x
-		coef = random.int(100, 120) / 100;
-	} else if (a <= 300) {
-		// 20%: 1.21x – 2.00x
-		coef = random.int(121, 200) / 100;
-	} else if (a <= 600) {
+	if (a <= 200) {
+		// 20%: 1.50x – 2.00x
+		coef = random.int(150, 200) / 100;
+	} else if (a <= 500) {
 		// 30%: 2.01x – 5.00x
 		coef = random.int(201, 500) / 100;
 	} else if (a <= 800) {
-		// 20%: 5.01x – 10.00x
+		// 30%: 5.01x – 10.00x
 		coef = random.int(501, 1000) / 100;
-	} else if (a <= 950) {
-		// 15%: 10.01x – 50.00x
-		coef = random.int(1001, 5000) / 100;
 	} else {
-		// 5%: 50.01x – 100.00x
-		coef = random.int(5001, 10000) / 100;
+		// 20%: 10.01x – 17.50x
+		coef = random.int(1001, 1750) / 100;
 	}
 
 	return coef;
