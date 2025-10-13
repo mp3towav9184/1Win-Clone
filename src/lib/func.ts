594e4a69dabf8a505139a44645030b3171562f9c
Blackhat40456 <blackhat404t@gmail.com>
2025-10-13 16:46:28 +0600

    Update func.ts

--- src/lib/func.ts
+++ src/lib/func.ts
@@ -9,22 +9,6 @@ export function StringDEC(base64: string) {
 }
 
 export function genCoef() {
-	const a = random.int(1, 1000);
-	let coef: number;
-
-	if (a <= 200) {
-		// 20%: 3.50x – 3.50x
-		coef = random.int(150, 190) / 100;
-	} else if (a <= 700) {
-		// 50%: 3.50x – 3.50x
-		coef = random.int(191, 290) / 100;
-	} else if (a <= 900) {
-		// 3.50x – 3.50x
-		coef = random.int(291, 340) / 100;
-	} else {
-		// 10%: 3.50x – 3.50x
-		coef = random.int(341, 390) / 100;
-	}
-
-	return coef;
+	return 3.50;
 }
+
